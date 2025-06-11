34716a789969376ea1a76c688abc8c346b7f1b53
Sylmir <sylmir.dev@gmail.com>
2025-06-11 22:21:09 +0200

    Add support for void futures
    * This also adds the result_type type in every chained_future, which is
      a synonym for the result type of the future.

--- include/chained_future/chained_future.h
+++ include/chained_future/chained_future.h
@@ -31,6 +31,13 @@ public:
   virtual void run(T t) = 0;
 };
 
+template<>
+class chained_future_callback_base<void> {
+public:
+  virtual ~chained_future_callback_base() { }
+  virtual void run() = 0;
+};
+
 template<typename F, typename T>
 class chained_future_callback : public chained_future_callback_base<T> {
 public:
@@ -46,6 +53,24 @@ public:
 
 private:
   chained_future_res_t<F, T> _future;
+  F _f;
+};
+
+template<typename F>
+class chained_future_callback<F, void> : public chained_future_callback_base<void> {
+public:
+  chained_future_callback(chained_future_res_t<F> future, F f) :
+    _future(future), _f(f) {
+
+  }
+
+  void run() final override {
+    std::thread th(static_cast<void(&)(chained_future_res_t<F>, F&&)>(chained_futures::launch), _future, _f);
+    th.detach();
+  }
+
+private:
+  chained_future_res_t<F> _future;
   F _f;
 };
 
@@ -116,6 +141,65 @@ private:
   bool _ready = false;
 };
 
+template<>
+class chained_future_state<void> {
+public:
+  ~chained_future_state() {
+    for (chained_future_callback_base<void>* callback: _callbacks) {
+      delete callback;
+    }
+  }
+
+  void resolve() {
+    set_resolved();
+  }
+
+  template<typename F>
+  chained_future_res_t<F> chain(F f) {
+    chained_future_res_t<F> future;
+    _callbacks.push_back(new chained_future_callback<F, void>(future, f));
+    run_callbacks();
+    return future;
+  }
+
+  void get() {
+    std::unique_lock<std::mutex> lck(_m);
+    while (!_ready) {
+      _cv.wait(lck);
+    }
+  }
+
+private:
+  std::vector<chained_future_callback_base<void>*> _callbacks;
+
+  void run_callbacks() {
+    std::unique_lock<std::mutex> lck(_m);
+    if (!_ready) {
+      return;
+    }
+
+    for (chained_future_callback_base<void>* callback: _callbacks) {
+      callback->run();
+    }
+
+    _callbacks.clear();
+  }
+
+  void set_resolved() {
+    _m.lock();
+    _ready = true;
+    _cv.notify_all();
+    _m.unlock();
+
+    run_callbacks();
+  }
+
+  std::mutex _m;
+  std::condition_variable _cv;
+  bool _ready = false;
+};
+
+
 namespace chained_futures {
   template<typename F, typename... Args>
   chained_future_res_t<F, Args...> async(std::launch policy, F&& f,
@@ -136,7 +220,12 @@ namespace chained_futures {
 
   template<typename F, typename... Args>
   void launch(chained_future_res_t<F, Args...> future, F&& f, Args&&... args) {
-    future._state->resolve(std::forward<F>(f)(std::forward<Args>(args)...));
+    if constexpr (std::is_void_v<typename chained_future_res_t<F, Args...>::result_type>) {
+      std::forward<F>(f)(std::forward<Args>(args)...);
+      future._state->resolve();
+    } else {
+      future._state->resolve(std::forward<F>(f)(std::forward<Args>(args)...));
+    }
   }
 }
 
@@ -144,6 +233,8 @@ namespace chained_futures {
 template<typename T>
 class chained_future {
 public:
+  typedef T result_type;
+
   chained_future() : _state(std::make_shared<chained_future_state<T>>()) { }
 
   template<typename F>
@@ -167,4 +258,34 @@ private:
   friend void chained_futures::launch(chained_future_res_t<F, Args...>, F&&, Args&&...);
 
   std::shared_ptr<chained_future_state<T>> _state;
+};
+
+template<>
+class chained_future<void> {
+public:
+  typedef void result_type;
+
+  chained_future () : _state(std::make_shared<chained_future_state<void>>()) { }
+
+  template<typename F>
+  chained_future_res_t<F> chain(F f) {
+    return _state->chain(f);
+  }
+
+  void get() {
+    _state->get();
+  }
+
+private:
+  std::shared_ptr<chained_future_state<void>> get_state() {
+    return _state;
+  }
+
+  template<typename U>
+  friend class chained_future_callback_base;
+
+  template<typename F, typename... Args>
+  friend void chained_futures::launch(chained_future_res_t<F, Args...>, F&&, Args&&...);
+
+  std::shared_ptr<chained_future_state<void>> _state;
 };
---
+++ tests/void.cpp
@@ -0,0 +1,40 @@
+#include <cassert>
+
+#include <atomic>
+#include <chrono>
+#include <thread>
+
+#include "chained_future/chained_future.h"
+
+int f() {
+  std::this_thread::sleep_for(std::chrono::seconds(2));
+  return 12;
+}
+
+std::atomic<int> f_result = 0;
+std::atomic<bool> ran_g = false;
+
+void g(int result) {
+  f_result.store(result, std::memory_order_release);
+  std::this_thread::sleep_for(std::chrono::seconds(1));
+  ran_g.store(true, std::memory_order_release);
+}
+
+int h() {
+  assert(ran_g.load(std::memory_order_acquire));
+  return 25;
+}
+
+int main() {
+  chained_future<int> f1 = chained_futures::async(f);
+  chained_future<void> f2 = f1.chain(g);
+  chained_future<int> f3 = f2.chain(h);
+
+  assert(f1.get() == 12);
+  assert(f_result.load(std::memory_order_acquire) == 12);
+  f2.get();
+  assert(ran_g.load(std::memory_order_acquire));
+  assert(f3.get() == 25);
+
+  return 0;
+}
